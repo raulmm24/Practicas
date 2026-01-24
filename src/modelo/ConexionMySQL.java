4e2451cba40d09ef86c408942070a0d5f97198bb
raulm <raulmartinezmartinez24@gmail.com>
2026-01-24 12:32:32 +0100

    Modificación y realización en las sigueintes clases

--- src/modelo/ConexionMySQL.java
+++ src/modelo/ConexionMySQL.java
@@ -6,25 +6,17 @@ import java.sql.SQLException;
 
 public class ConexionMySQL {
 
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String BBDD = "jdbc:mysql://localhost:3306/?user=root";
-    private static final String DB_NAME = "practicas";
+    private static final String URL = "jdbc:mysql://localhost:3306/practicas";
     private static final String USUARIO = "root";
     private static final String PASSWORD = "774411";
 
-    public static void main(String [] args) {}
-
     public Connection conexionBBDD() {
-        Connection conexion = null;
         try {
-            Class.forName(DRIVER);
-            conexion = DriverManager.getConnection(BBDD + DB_NAME, USUARIO, PASSWORD);
-        } catch (ClassNotFoundException e) {
-            System.err.println("Se ha producido un error en Driver.\n" + e);
+            return DriverManager.getConnection(URL, USUARIO, PASSWORD);
         } catch (SQLException e) {
             System.err.println("Se ha producido un error al conectar la base de datos.\n" + e);
+            return null;
         }
-        return conexion;
     }
 
     public void cerrarConexion(Connection conexion) {
@@ -33,7 +25,7 @@ public class ConexionMySQL {
                 conexion.close();
             }
         } catch (SQLException e) {
-            System.err.println("Se ha producido un error al cerrar la conexion con la base de datos." + e);
+            System.err.println("Se ha producido un error al cerrar la conexion con la base de datos.\n" + e);
         }
     }
 }
